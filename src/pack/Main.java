44f9abdad1a8b612e1984f4502957b879cb1f735
ApComp <williamfath@hs-lab-4-11.local>
2016-10-06 09:59:12 -0600

    1.2

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	private static int[][] compboard = new int[10][10];
 	private static int[][] dummy = new int[10][10];
 	private static UserInput user = new UserInput();
-	
+	private static boolean game = true;
 
 	//Boards
 	public static int playerBoard(){
@@ -46,16 +46,23 @@ public class Main {
 		compboard[randomRow][randomColumn] = 1;
 	}
 	public static void playerShip(){
+		
 		int shipRow =(int) user.getDoubleInput("Please input a row number: 0-9");
 		int shipColumn = (int) user.getDoubleInput("Please input a column number: 0-9");
-		board[shipRow][shipColumn] = 1;
+		if((shipRow <= 9 && shipColumn <= 9) && (shipRow >= 0 && shipColumn >= 0) ) {
+			board[shipRow][shipColumn] = 1;
+		}else{
+			System.out.println("Question your intelligence.");
+			playerShip();
+		}
 	}
-	public static void fire(){
+		public static void fire(){
 		int rowFire =(int) user.getDoubleInput("Please input a row to fire upon.");
 		int columnFire =(int) user.getDoubleInput("Please input a column to fire upon.");
 		if(compboard[rowFire][columnFire] == 1){
 			compboard[rowFire][columnFire] =7;
 			System.out.println("You are Winner!");
+			game = false;
 		}else if(compboard[rowFire][columnFire] != 1){
 			System.out.println("You have missed the objective.");
 		}
@@ -68,11 +75,14 @@ public class Main {
 		compBoard();
 		playerShip();
 		playerBoard();
-		fire();
+		while (game){
+			fire();
+		}
 		
 		
 		
 		
+	
 		
 		
 		
